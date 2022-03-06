eafe304edd19c289b52ef8fd500e53ea895df633
Eduardo-22002197 <a22002197@alunos.ulht.pt>
2022-03-06 19:56:06 +0000

    Changed PessoaSemCartaException. Now verifying if the person trying to move the vehicle has licence.

--- src/pt/ulusofona/cm/kotlin/challenge/exceptions/PessoaSemCartaException.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/exceptions/PessoaSemCartaException.kt
@@ -1,4 +1,4 @@
 package pt.ulusofona.cm.kotlin.challenge.exceptions
 
-class PessoaSemCartaException (override val message: String = "No drivers license!"): Exception(message) {
+class PessoaSemCartaException (var name: String, override val message: String = "$name não tem carta para conduzir o veículo indicado"): Exception(message) {
 }
